f6b479590203315764978a7c7836540faf88de81
Anthony Liu <ymliu173@gmail.com>
2016-04-08 09:43:26 +0800

    add intent keys

---
+++ app/src/main/java/com/anthony/imageprocessing/IntentKeys.java
@@ -0,0 +1,11 @@
+package com.anthony.imageprocessing;
+
+/**
+ * Created by anthonyliu on 2016/4/8.
+ */
+public class IntentKeys {
+
+    public static final String REPLACED_COLOR = "replacedColor";
+    public static final String REPLACING_COLOR = "replacingColor";
+
+}
